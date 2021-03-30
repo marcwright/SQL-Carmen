21079f438e1c58a760020a5513a2f0f605166d6e
Quincy Elery <quincyelery@gmail.com>
2021-03-30 18:07:41 -0500

    clue 2 answered

--- find_carmen.sql
+++ find_carmen.sql
@@ -8,10 +8,14 @@ WHERE region = 'Southern Europe'
 ORDER BY population
 LIMIT 1;
 
+
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
 
+SELECT * 
+FROM countrylanguage
+WHERE countrycode = 'VAT';
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
@@ -24,7 +28,6 @@ LIMIT 1;
  -- There are only two cities she could be flying to in the country. One is named the same as the country – that
  -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
  --  be flying to.
-
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
