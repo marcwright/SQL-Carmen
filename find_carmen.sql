786d4161d844f462abc77e3681edd1449fd88f09
Quincy Elery <quincyelery@gmail.com>
2021-03-30 18:35:03 -0500

    clue 6 answered

--- find_carmen.sql
+++ find_carmen.sql
@@ -52,7 +52,7 @@ SELECT countrycode
 FROM city
 WHERE name LIKE 'Serra%' AND name <> 'Serravalle';
 
-SELECT name
+SELECT name, capital
 FROM country
 WHERE code = 'BRA';
 
@@ -61,6 +61,10 @@ WHERE code = 'BRA';
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
 -- follow right behind you!
+
+SELECT name
+FROM city 
+WHERE id = 211;
 
 
 
