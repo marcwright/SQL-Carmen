b15530dddea1027e3a230ed7b0008e005199b05f
Quincy Elery <quincyelery@gmail.com>
2021-03-30 18:40:25 -0500

    clue 7 answered, final city answered

--- find_carmen.sql
+++ find_carmen.sql
@@ -83,6 +83,12 @@ WHERE id = 211;
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
+SELECT name, district, countrycode
+FROM city
+WHERE population = 91084;
 
 
--- She's in ____________________________!
+
+
+
+-- She's in Santa Monica, California, USA"!
