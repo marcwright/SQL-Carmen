1cca5e8da43a449320f3c6502d4286134b0eee8a
Viktor Olesnevych <viktor.olesnevych@hotmail.com>
2021-03-31 03:21:28 -0500

    Clue #2

--- script.sql
+++ script.sql
@@ -1,3 +1,9 @@
 SELECT name FROM country
 WHERE population = (SELECT MIN(population) FROM country 
 WHERE region = 'Southern Europe');
+
+SELECT language FROM countrylanguage
+WHERE isofficial = true
+AND countrycode = (SELECT code FROM country 
+					WHERE name LIKE '%Holy See%');
+					
