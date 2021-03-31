fd6e6c761aa36a0c84c202cd609d42569f94f070
Alvin Muniz <muniz.alvin@gmail.com>
2021-03-31 06:50:43 -0500

    Problem 5 and 6

--- find_carmen.sql
+++ find_carmen.sql
@@ -30,13 +30,15 @@ NAME NOT LIKE 'San Marino';
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
+SELECT * FROM city
+WHERE name LIKE '%San M%';
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
  -- follow right behind you!
 
-
+SELECT country.capital FROM country WHERE code = 'ARG';
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
  -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
